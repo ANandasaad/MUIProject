46699b12a0f4924b634435838159fe546acdef3d
Anand kushwaha <anandkushwaha70@gmail.com>
2022-09-17 01:18:37 +0530

    MUI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { AppBar, Toolbar, styled, Typography, InputBase, Badge  } from '@mui/material'
+import { AppBar, Toolbar, styled, Typography, InputBase, Badge, Avatar, MenuItem, Menu  } from '@mui/material'
 import MailIcon from '@mui/icons-material/Mail';
 import React from 'react'
 import WebhookIcon from '@mui/icons-material/Webhook';
 import { Box } from '@mui/system';
 import NotificationsIcon from '@mui/icons-material/Notifications';
+import { useState } from 'react';
 
 const StyledToolbar = styled(Toolbar)({
     display: "flex",
@@ -18,9 +19,25 @@ const Search = styled("div")(({ theme }) => ({
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
+    display:"none",
+    alignItems:"center",
+    gap:"20px",
+    [theme.breakpoints.up("sm")]:{
+        display:"flex"
+    }
+    
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+    display:"flex",
+    alignItems:"center",
+    gap:"10px",
+    [theme.breakpoints.up("sm")]:{
+        display:"none"
+    }
     
 }));
 const Navbar = () => {
+    const [open, setOpen]= useState(false)
     return (
         <AppBar position='sticky'><StyledToolbar>
             <Typography variant='h6' sx={{ display: { xs: "none", sm: "block" } }}>
@@ -34,9 +51,30 @@ const Navbar = () => {
             <Badge badgeContent={4} color="error">
                 <NotificationsIcon/>
             </Badge>
-            
+            <Avatar alt="Remy Sharp" src="https://www.pexels.com/photo/pluto-costume-3411134/" onClick={(e)=>setOpen(true)} />
             </Icons>
+            <UserBox onClick={(e)=> setOpen(true)}>
+            <Avatar alt="Remy Sharp" src="https://www.pexels.com/photo/pluto-costume-3411134/"/>
+            </UserBox>
         </StyledToolbar>
+        <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e)=> setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
         </AppBar>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,74 @@
-import { Box } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React from 'react'
-
+import HomeIcon from '@mui/icons-material/Home';
+import SubjectIcon from '@mui/icons-material/Subject';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import PeopleIcon from '@mui/icons-material/People';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 const Sidebar = () => {
   return (
-    <Box bgcolor="skyblue" flex={1} padding={2} sx={{ display :{xs: "none",sm:"block"}}} >Sidebar</Box>
+    <Box  flex={1} padding={2} sx={{ display :{xs: "none",sm:"block"}}} >
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <HomeIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Homepage"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#page">
+              <ListItemIcon>
+                <SubjectIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Pages"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Groups">
+              <ListItemIcon>
+                <GroupsIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Groups"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#marketplace">
+              <ListItemIcon>
+                <StorefrontIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Marketplace"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#friends">
+              <ListItemIcon>
+                <PeopleIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Friends"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#settings">
+              <ListItemIcon>
+                <SettingsIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Settingd"/>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#profile">
+              <ListItemIcon>
+                <AccountBoxIcon/>
+              </ListItemIcon>
+              <ListItemText primary="Profile"/>
+            </ListItemButton>
+          </ListItem>
+          </List>
+    </Box>
   )
 }
 
